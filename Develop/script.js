840c8b4921d967a59c17535fedd8b22a805ea0ec
jeremysr <jeremysr@protonmail.com>
2023-07-01 16:46:32 -0600

    finished the number generator, and figured out how to store its results to an array

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
  // Assignment code here
 //lets declare all the things
-var randomNumber = 0;
+var randomNumber = [];
 var lowerNumber = 0;
 var upperNumber = 0;
 var specialNumber = 0;
@@ -14,11 +14,13 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  console.log(password);
 }
 
-//lets define all the function logic
+//lets define all the function logic below
+
 //this will actually create the password
+//here we want to generate a character from a random number, and add to the password
+//and repeat that until it reaches the password length requested by user
 function generatePassword(){
   var alphaNumberic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numeric = "0123456789";
@@ -28,19 +30,35 @@ function generatePassword(){
   const upperCase = alphaNumberic.split("");
   const lowerCase = lower.split(""); 
   const specialChar = specials.split("");
-  const mathChars = [upperCase.length, lowerCase.length, specialChar.length]; 
+  const mathChars = [upperCase.length,  lowerCase.length, specialChar.length]; 
   randomNumberGen(0, mathChars);
+
+  //sets some variables to blank at the end so when this function executes again
+  //they don't include the previous result
+  randomNumber = [];
 }
 //random number generator
 //will pick a number between 0 and the length of each of the char arrays above
 //will return a number for each of the possibly needed char groups
 function randomNumberGen (min, max) {
   min = 0;
-  //I got the following equation from w3schools
+  console.log(min, max);
+  //I got the following equation from w3schools and put it in a for loop
   //this should generate the number we need for each of the array groups above
-  var tempNumber = Math.floor(
-    Math.random() * (max - min + 1) ) + min;
-  
+  function numberCalc() {
+    var tempNumber = 0;
+    function randomMath() {
+      tempNumber = Math.floor(Math.random() * (max[i] - min + 1) ) + min;
+      return tempNumber;
+    }
+    for (i = 0; i < max.length; i++) {
+      randomNumber.push(randomMath());
+    };
+    return randomNumber;
+  }
+  numberCalc();
+  console.log(randomNumber);
+  return randomNumber;
 }
 
 
--- README.md
+++ README.md
@@ -10,3 +10,5 @@ this helped me with random numbers:
 https://www.w3schools.com/JS/js_random.asp
 this helped me set up the git in the beginning, i did it backwards:
 https://superuser.com/questions/1412078/bring-a-local-folder-to-remote-git-repo
+this helped me figure out how to store a for loop result to an array:
+https://stackoverflow.com/questions/11455430/javascript-storing-for-loop-result
