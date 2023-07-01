1c9dafcdbdff55fc883326c0fadbff90c545eab9
jeremysr <jeremysr@protonmail.com>
2023-07-01 15:08:27 -0600

    added framework for a password generator, that will be derived from a random number generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
-// Assignment code here
-
+ // Assignment code here
+//lets declare all the things
+var randomNumber = 0;
+var lowerNumber = 0;
+var upperNumber = 0;
+var specialNumber = 0;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +14,35 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  console.log(password);
 }
+
+//lets define all the function logic
+//this will actually create the password
+function generatePassword(){
+  var alphaNumberic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numeric = "0123456789";
+  //TODO: figure out how to add " and ' into the string below
+  var specials = " !#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+  var lower = alphaNumberic.toLowerCase();
+  const upperCase = alphaNumberic.split("");
+  const lowerCase = lower.split(""); 
+  const specialChar = specials.split("");
+  const mathChars = [upperCase.length, lowerCase.length, specialChar.length]; 
+  randomNumberGen(0, mathChars);
+}
+//random number generator
+//will pick a number between 0 and the length of each of the char arrays above
+//will return a number for each of the possibly needed char groups
+function randomNumberGen (min, max) {
+  min = 0;
+  //I got the following equation from w3schools
+  //this should generate the number we need for each of the array groups above
+  var tempNumber = Math.floor(
+    Math.random() * (max - min + 1) ) + min;
+  
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 
 source code:
 https://github.com/coding-boot-camp/friendly-parakeet
+
+this helped me with random numbers:
+https://www.w3schools.com/JS/js_random.asp
+this helped me set up the git in the beginning, i did it backwards:
+https://superuser.com/questions/1412078/bring-a-local-folder-to-remote-git-repo
