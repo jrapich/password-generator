a1a0bc28f64c6d11532a57ee97c589cd85f87515
jeremysr <jeremysr@protonmail.com>
2023-07-01 18:24:22 -0600

    created a function to pick a random character based on the random number that is generated. moved most of the variables to the top of the code, and to make it easier to debug individual functions, and to make sure the logic is working correctly so far. it seems to be working well.

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,18 @@
  // Assignment code here
 //lets declare all the things
+var alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+//TODO: figure out how to add " and ' into the string below without it messing up
+var specials = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+var lower = alphaNumeric.toLowerCase();
+const upperCase = alphaNumeric.split("");
+const lowerCase = lower.split(""); 
+const specialChar = specials.split("");
+const numeric = numbers.split("");
+const mathChars = [upperCase.length -1,  lowerCase.length -1, specialChar.length - 1, numeric.length -1];
 var randomNumber = [];
-var lowerNumber = 0;
-var upperNumber = 0;
-var specialNumber = 0;
+var tempNumber = 0;
+var singleChar = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -12,8 +21,14 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
+  //sets some arrays/vars to blank at the end
+  //so when the user clicks the button and this generator executes again
+  //they don't include the previous result
+  randomNumber = [];
+  tempNumber = 0;
+  singleChar = "";
 }
 
 //lets define all the function logic below
@@ -21,32 +36,18 @@ function writePassword() {
 //this will actually create the password
 //here we want to generate a character from a random number, and add to the password
 //and repeat that until it reaches the password length requested by user
-function generatePassword(){
-  var alphaNumberic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numeric = "0123456789";
-  //TODO: figure out how to add " and ' into the string below
-  var specials = " !#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-  var lower = alphaNumberic.toLowerCase();
-  const upperCase = alphaNumberic.split("");
-  const lowerCase = lower.split(""); 
-  const specialChar = specials.split("");
-  const mathChars = [upperCase.length,  lowerCase.length, specialChar.length]; 
-  randomNumberGen(0, mathChars);
-
-  //sets some variables to blank at the end so when this function executes again
-  //they don't include the previous result
-  randomNumber = [];
+function generatePassword(){ 
+  passHasher(randomNumberGen(0, mathChars));
+  return password;
 }
 //random number generator
 //will pick a number between 0 and the length of each of the char arrays above
 //will return a number for each of the possibly needed char groups
 function randomNumberGen (min, max) {
   min = 0;
-  console.log(min, max);
   //I got the following equation from w3schools and put it in a for loop
   //this should generate the number we need for each of the array groups above
   function numberCalc() {
-    var tempNumber = 0;
     function randomMath() {
       tempNumber = Math.floor(Math.random() * (max[i] - min + 1) ) + min;
       return tempNumber;
@@ -57,8 +58,34 @@ function randomNumberGen (min, max) {
     return randomNumber;
   }
   numberCalc();
-  console.log(randomNumber);
+  console.log(" the random number array is " + randomNumber);
   return randomNumber;
+}
+//character picker based on random number, will pass one random char
+function passHasher(num) {
+  var charPicker = ["upper", "lower", "special", "number"];
+  charPicker = charPicker[Math.floor(Math.random() * charPicker.length)];
+  tempNumber = Math.floor(Math.random() * num.length);
+  tempNumber = randomNumber[tempNumber];
+  if (charPicker === "upper") {
+    singleChar = upperCase[randomNumber[0]];
+    console.log("random number picked for char group is: " + randomNumber[0]);
+  } 
+  if (charPicker === "lower") {
+    singleChar = lowerCase[randomNumber[1]];
+    console.log("random number picked for char group is: " + randomNumber[1]);
+  }
+  if (charPicker === "special") {
+    singleChar = specialChar[randomNumber[2]];
+    console.log("random number picked for char group is: " + randomNumber[2]);
+  }
+  if (charPicker === "number") {
+    singleChar = numeric[randomNumber[3]];
+    console.log("random number picked for char group is: " + randomNumber[3]);
+  }
+  console.log("char group picked is: " + charPicker);
+  console.log("random char generated is: " + singleChar);
+  return singleChar;
 }
 
 
